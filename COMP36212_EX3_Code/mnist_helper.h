a3282c78e0a29f2d7a624c5b289047f5cb752ee5
Shivsaransh <Shivsaransh1@yahoo.com>
2025-05-01 17:22:39 +0100

    fix: commit fread checks and CI config

--- COMP36212_EX3_Code/mnist_helper.h
+++ COMP36212_EX3_Code/mnist_helper.h
@@ -5,8 +5,8 @@
 
 #define N_TRAINING_SET 60000
 #define N_TESTING_SET 10000
-#define PIXEL_DIM 28 // MNIST input array is 28 by 28 pixels
-#define PIXEL_DIM_FLAT 784 // 784 = 28 * 28
+#define PIXEL_DIM 28        // MNIST input array is 28 by 28 pixels
+#define PIXEL_DIM_FLAT 784  // 784 = 28 * 28
 
 extern uint8_t** training_data;
 extern uint8_t training_labels[N_TRAINING_SET];
@@ -22,7 +22,4 @@ void free_dataset_data_structures(void);
 void print_single_example(uint8_t** dataset, int n);
 void print_string(char* words);
 
-
 #endif /* MNIST_HELPER_H */
-
-
